6d65cdd6b5b90cf2415b069648da15fe8b785f5e
Paul Cummings <paul.cummings@vodafone.com>
2026-04-20 10:24:15 +0100

    feat: update version to 0.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import find_packages, setup
 
-
 setup(
     name="gitlab-dr",
-    version="0.1.0",
+    version="0.2.0",
     description="GitLab disaster recovery backup/restore CLI",
     long_description="Backup and restore GitLab groups/projects to zip archives.",
     long_description_content_type="text/plain",
